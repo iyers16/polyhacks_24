c27f93d1a11c4872a7f1e87f169360d5b0b49083
iyers16 <shanmukh.iyer@gmail.com>
2024-02-03 19:18:46 -0500

    some side bar routing

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+// MapPage.jsx
+import React, { useState } from 'react';
+// import Map from './Map';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Sidebar from '../components/SideBar';
 
 const MapPage = () => {
+    const [sidebarVisible, setSidebarVisible] = useState(false);
+
+    const toggleSidebar = () => {
+        setSidebarVisible(!sidebarVisible);
+    };
 
     return (
         <div>
-            <h2>Map Page</h2>
+            <Sidebar visible={sidebarVisible} toggle={toggleSidebar} />
+            <button className="toggle-button" onClick={toggleSidebar}>
+                <FontAwesomeIcon icon={faBars} />
+            </button>
+            {/* <Map /> */}
         </div>
     );
 };
