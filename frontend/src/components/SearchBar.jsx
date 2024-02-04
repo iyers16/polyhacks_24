ffd9eb8b59a650b312400c64c9e5322673818a91
Zineb2a <zineb.alaoui663@gmail.com>
2024-02-03 22:00:54 -0500

    searchbar

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,67 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+function SearchBar({ onPlaceSelected }) {
+  const [autocomplete, setAutocomplete] = useState(null);
+  const autocompleteInput = useRef(null);
+
+  useEffect(() => {
+    const loadGoogleMapsScript = (callback) => {
+      if (window.google) {
+        callback();
+        return;
+      }
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
+      document.head.append(script);
+      script.onload = () => {
+        callback();
+      };
+    };
+
+    loadGoogleMapsScript(() => {
+      setAutocomplete(new window.google.maps.places.Autocomplete(autocompleteInput.current));
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!autocomplete) return;
+
+    const placeChangedListener = autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+      if (place.geometry) {
+        onPlaceSelected(place);
+      } else {
+        console.log("Returned place contains no geometry");
+      }
+    });
+
+    return () => {
+      window.google.maps.event.removeListener(placeChangedListener);
+    };
+  }, [autocomplete, onPlaceSelected]);
+
+  return (
+    <input
+      ref={autocompleteInput}
+      type="text"
+      placeholder="Enter a location"
+      style={{
+        width: '500px',
+        height: '40px',
+        position: 'absolute',
+        top: '10px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        zIndex: '5',
+        padding: '10px',
+        border: '1px solid #ccc',
+        borderRadius: '20px',
+        boxShadow: '0 2px 6px rgba(0,0,0,0.3)',
+        fontSize: '16px',
+        outline: 'none',
+      }}
+    />
+  );
+}
+
+export default SearchBar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,23 @@
-// Home.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import SearchBar from "../components/SearchBar";
+
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handlePlaceSelected = (place) => {
+        // Logic to handle the selected place
+        // For example, you might want to navigate to the MapPage with the selected place's details
+        navigate('/map', { state: { selectedPlace: place } });
+    };
+
     return (
         <div>
             <div>
                 <h2>Welcome to our Google Maps App</h2>
+                {/* Include the SearchBar component and handle the selected place */}
+                <SearchBar onPlaceSelected={handlePlaceSelected} />
                 <p>Click <Link to="/map">here</Link> to view the map.</p>
             </div>
         </div>
